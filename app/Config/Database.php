bdd70fc1ec3f7bc697f02b51770073dc91e3ce55
RasyiidWho <rasyiidwho@gmail.com>
2022-09-29 23:46:38 +0700

    ngubah detail db mergo pw salah

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -34,7 +34,7 @@ class Database extends Config
         'DSN'      => '',
         'hostname' => 'localhost',
         'username' => 'root',
-        'password' => '',
+        'password' => 'rasyiid123',
         'database' => 'idea',
         'DBDriver' => 'MySQLi',
         'DBPrefix' => '',
